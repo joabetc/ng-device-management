c028115cc97777743773817144201c6dd561f55f
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-03-07 11:55:52 -0300

    Creating a failing test

--- src/app/device.service.spec.ts
+++ src/app/device.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DeviceService } from './device.service';
+import { Device } from './model/device';
 
 describe('DeviceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -9,4 +10,14 @@ describe('DeviceService', () => {
     const service: DeviceService = TestBed.get(DeviceService);
     expect(service).toBeTruthy();
   });
+
+  it('should return device list', (done: DoneFn) => {
+    const service: DeviceService = TestBed.get(DeviceService);
+    service.getDevices().subscribe({
+      next: (devices: Device[]) => {
+        expect(devices).toBeTruthy();
+        done();
+      }
+    })
+  })
 });
