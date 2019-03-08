26b37ecfd572a7d3f8d556f5b20f9d871e3209dd
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-03-08 15:39:05 -0300

    Enabling Firebase and adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,29 @@ import { UsersComponent } from './users/users.component';
 import { MatTableModule, MatSortModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { EditComponent } from './devices/edit/edit.component';
+import { ListComponent } from './devices/list/list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     DevicesComponent,
-    UsersComponent
+    UsersComponent,
+    EditComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
     MatSortModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
