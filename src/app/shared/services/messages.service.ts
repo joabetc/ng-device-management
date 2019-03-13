6d3f0ef4d09b0cf9fca88ad7fdff1326de01f65c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-03-13 11:32:48 -0300

    Updating service implementation with new interface

--- src/app/shared/services/messages.service.ts
+++ src/app/shared/services/messages.service.ts
@@ -1,15 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Message } from '../model/message';
+import { MessageTypes } from '../model/message-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
-  messages: string[] = [];
+  messages: Message[] = [];
 
   constructor() { }
 
-  add(message: string) {
-    this.messages.push(message);
+  private add(type: MessageTypes, message: string) {
+    let msg: Message = {
+      type: type,
+      message: message
+    }
+    this.messages.push(msg);
+  }
+
+  addSuccess(message: string) {
+    this.add(MessageTypes.SUCESS, message);
+  }
+
+  addInfo(message: string) {
+    this.add(MessageTypes.INFO, message);
+  }
+
+  addWarning(message: string) {
+    this.add(MessageTypes.WARNING, message);
+  }
+
+  addError(message: string) {
+    this.add(MessageTypes.DANGER, message);
+  }
+
+  remove(index: number) {
+    this.messages.splice(index, 1);
   }
 
   clear() {
