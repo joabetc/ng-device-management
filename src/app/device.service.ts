8c2098878c6cbfb19847fe62a2e12d35358a4c45
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-03-13 11:33:30 -0300

    Adding message service to device service

--- src/app/device.service.ts
+++ src/app/device.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from '@angular/core';
 import { Device } from './model/device';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
+import { MessagesService } from './shared/services/messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeviceService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(
+    private db: AngularFireDatabase,
+    private messagesServices: MessagesService) { }
 
   insert(device: Device) {
     this.db.list('device')
       .push(device)
+      .then((result: any) => {
+        this.messagesServices.addSuccess(`Device ${device.name} sucessfully saved!`);
+      })
       .then((result: any) => {
         console.log(result.key);
       });
@@ -22,6 +28,7 @@ export class DeviceService {
     this.db.list('device')
       .update(key, device)
       .catch((error: any) => {
+        this.messagesServices.addError(`An unexpected error ocurred while updating ${device.name}`);
         console.error(error);
       })
   }
