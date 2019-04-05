3f17277bed150dec62d84f5ea8b5a704124e3a72
Joabe Tavares Costa <jtcosta@Joabe.local>
2019-04-05 17:09:23 -0300

    Removing unused import #12

--- src/app/device-api.service.ts
+++ src/app/device-api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiResult } from './shared/model/api-result';
 import { Observable } from 'rxjs';
-import { BADNAME } from 'dns';
 
 const BRAND_DATA = 'assets/data/brands.json';
 
