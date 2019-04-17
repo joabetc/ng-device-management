84e1b6bd550788f5d041a3c3f2624b525c163c47
Joabe Costa <joabetc@hotmail.com>
2019-04-17 20:10:21 -0300

    Creating an adapter for Device class #21

---
+++ src/app/shared/device.adapter.ts
@@ -0,0 +1,25 @@
+import { Adapter } from './adapter';
+import { Device } from '../model/device';
+
+export class DeviceAdapter implements Adapter<Device> {
+
+  adaptFrom(item: any): Device {
+    const device = new Device();
+    device.assetNumber = item.assetNumber;
+    device.brand = item.brand;
+    device.model = item.model;
+    device.name = item.name;
+    device.os = item.os;
+    return device;
+  }
+
+  adaptTo(device: Device): any {
+    return {
+      assetNumber: device.assetNumber,
+      brand: device.brand,
+      model: device.model,
+      name: device.name,
+      os: device.os
+    }
+  }
+}
