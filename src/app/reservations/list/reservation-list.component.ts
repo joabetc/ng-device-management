1faa3afb86edec7a9b769835a1778bfc5a757d0a
SCOPUS\jtcosta <joabe.tc@gmail.com>
2019-05-09 17:12:30 -0300

    Hiding button for other users than the owner #24

--- src/app/reservations/list/reservation-list.component.ts
+++ src/app/reservations/list/reservation-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ReservationService } from 'src/app/reservation.service';
 import { ReservationTable } from 'src/app/model/reservation-table';
 import { ReservationDataService } from '../shared/reservation-data.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'reservation-list',
@@ -16,7 +17,8 @@ export class ReservationListComponent implements OnInit {
 
   constructor(
     private reservationService: ReservationService,
-    private reservatioDataService: ReservationDataService
+    private reservatioDataService: ReservationDataService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() { }
@@ -27,5 +29,9 @@ export class ReservationListComponent implements OnInit {
 
   edit(reservation: ReservationTable, key: string) {
     this.reservatioDataService.changeReservation(reservation, key);
+  }
+
+  canChange(uid: string) {
+    return this.authService.getCurrentUser().uid === uid;
   }
 }
