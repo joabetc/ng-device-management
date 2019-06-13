6c80412a1cd1671488476205f455fdfd165dae36
Joabe Costa <joabetc@hotmail.com>
2019-06-12 23:20:56 -0300

    Hiding alert automatically #35

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,22 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { MessagesService } from '../shared/services/messages.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { timer } from 'rxjs';
+import { Alert } from '../shared/model/alert';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        opacity: 1
+      })),
+      state('closed', style({
+        opacity: 0
+      })),
+      transition('open => closed', [
+        animate('5s')
+      ])
+    ])
+  ]
 })
 export class MessagesComponent implements OnInit {
+  isOpen = true;
+
+  messages: Alert[] = [];
 
   constructor(
     public messageService: MessagesService
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.messageService.getAlerts().subscribe((alert: Alert) => {
+      if (!alert) {
+        this.messages = [];
+        return;
+      }
+
+      alert.visible = true;
+      this.messages.push(alert);
+      timer(5000).subscribe(i => {
+        this.messages.find(x => x === alert).visible = false;
+      });
+      timer(10000).subscribe(i => {
+        console.log('removing');
+        this.messages = this.messages.filter(x => x !== alert);
+      });
+    });
   }
 
   removeMessage(index: number) {
     this.messageService.remove(index);
+    this.messages.splice(index, 1);
   }
 
 }
--- src/app/shared/services/messages.service.ts
+++ src/app/shared/services/messages.service.ts
@@ -1,21 +1,41 @@
 import { Injectable } from '@angular/core';
 import { Message } from '../model/message';
 import { MessageTypes } from '../model/message-types';
+import { Observable, Subject } from 'rxjs';
+import { Router, NavigationStart } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
+  message = new Subject<Message>();
+  private keepAfterRouteChange = false;
+
   messages: Message[] = [];
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        if (this.keepAfterRouteChange) {
+          this.keepAfterRouteChange = false;
+        } else {
+          this.clear();
+        }
+      }
+    });
+  }
+
+  getAlerts(): Observable<any> {
+    return this.message.asObservable();
+  }
 
   private add(type: MessageTypes, message: string) {
-    let msg: Message = {
-      type: type,
-      message: message
-    }
+    const msg: Message = {
+      type,
+      message
+    };
     this.messages.push(msg);
+    this.message.next({ type, message } as Message);
   }
 
   addSuccess(message: string) {
